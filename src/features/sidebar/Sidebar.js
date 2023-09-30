86da60fedd21eb4fb4968655d224da6f1e4912a6
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-30 19:52:29 +0530

    redesign side bar

--- src/features/sidebar/Sidebar.js
+++ src/features/sidebar/Sidebar.js
@@ -16,76 +16,44 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
-      <div className="sidebar">
-        <div className="top">
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <span className="logo">Travel World</span>
-          </Link>
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">Travel World</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon" />
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PersonOutlineIcon className="icon" />
+                            <span>All Users</span>
+                        </li>
+                    </Link>
+                    <p className="title">USER</p>
+                    <Link to="/user-profile" style={{ textDecoration: "none" }}>
+                        <li>
+                            <AccountCircleOutlinedIcon className="icon" />
+                            <span>User Profile</span>
+                        </li>
+                    </Link>
+                    <Link to="/logout" style={{ textDecoration: "none" }}>
+                        <li>
+                            <ExitToAppIcon className="icon" />
+                            <span>Logout</span>
+                        </li>
+                    </Link>
+                </ul>
+            </div>
         </div>
-        <hr />
-        <div className="center">
-          <ul>
-            <p className="title">MAIN</p>
-            <li>
-              <DashboardIcon className="icon" />
-              <span>Dashboard</span>
-            </li>
-            <p className="title">LISTS</p>
-            <Link to="/users" style={{ textDecoration: "none" }}>
-              <li>
-                <PersonOutlineIcon className="icon" />
-                <span>Users</span>
-              </li>
-            </Link>
-            <Link to="/products" style={{ textDecoration: "none" }}>
-              <li>
-                <StoreIcon className="icon" />
-                <span>Products</span>
-              </li>
-            </Link>
-            <li>
-              <CreditCardIcon className="icon" />
-              <span>Orders</span>
-            </li>
-            <li>
-              <LocalShippingIcon className="icon" />
-              <span>Delivery</span>
-            </li>
-            <p className="title">USEFUL</p>
-            <li>
-              <InsertChartIcon className="icon" />
-              <span>Stats</span>
-            </li>
-            <li>
-              <NotificationsNoneIcon className="icon" />
-              <span>Notifications</span>
-            </li>
-            <p className="title">SERVICE</p>
-            <li>
-              <SettingsSystemDaydreamOutlinedIcon className="icon" />
-              <span>System Health</span>
-            </li>
-            <li>
-              <PsychologyOutlinedIcon className="icon" />
-              <span>Logs</span>
-            </li>
-            <li>
-              <SettingsApplicationsIcon className="icon" />
-              <span>Settings</span>
-            </li>
-            <p className="title">USER</p>
-            <li>
-              <AccountCircleOutlinedIcon className="icon" />
-              <span>Profile</span>
-            </li>
-            <li>
-              <ExitToAppIcon className="icon" />
-              <span>Logout</span>
-            </li>
-          </ul>
-        </div>
-      </div>
     );
-  };
+};
 
 export default Sidebar
