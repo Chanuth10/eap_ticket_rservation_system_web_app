1b40e85ea97a6dd79b162f8b77aced78467bfbd4
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-10-01 16:23:24 +0530

    login api create

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -6,26 +6,27 @@ import {
   LOGIN_FAIL,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
+  LOAD_USER_FAIL,
   REGISTER_USER_FAIL,
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
 } from "../constans/userContants";
 
 // Login
-export const login = (email, password) => async (dispatch) => {
+export const login = (NIC, password) => async (dispatch) => {
   try {
     dispatch({ type: LOGIN_REQUEST });
 
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `/api/v2/login`,
-      { email, password },
+      `/api/Authentication`,
+      { NIC, password },
       config
     );
-    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+    dispatch({ type: LOGIN_SUCCESS, payload: data });
   } catch (error) {
-    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+    dispatch({ type: LOGIN_FAIL, payload: error.response });
   }
 };
 
--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useEffect, useRef, useState}from 'react'
 import { Button, Container, Row, Col, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors, login } from "../../../actions/userAction";
 import './login.css'
 
 import loginImg from '../../../assets/images/login.png'
 import userIcon from '../../../assets/images/user.png'
 
 const Login = () => {
+  const dispatch = useDispatch();
+  
+  const [loginNIC, setLoginNIC] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(loginNIC, loginPassword));
+  };
+
   return (
     <Container>
       <Row>
@@ -21,12 +33,12 @@ const Login = () => {
               </div>
               <h2>Login</h2>
 
-              <Form>
+              <Form onSubmit={loginSubmit}>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Control type="NIC" placeholder="NIC"  />
+                  <Form.Control type="NIC" placeholder="Enter user NIC" value={loginNIC} onChange={(e)=>setLoginNIC(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control type="password" placeholder="Enter user Password" value={loginPassword} onChange={(e)=>setLoginPassword(e.target.value)} />
                 </Form.Group>
                 <Button className='btn secondary__btn auth__btn' type='submit'>Login</Button>
               </Form>
