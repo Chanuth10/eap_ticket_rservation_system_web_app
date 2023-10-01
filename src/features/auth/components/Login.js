56199397c05eb9b522de845ff1ea0528824d9222
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-10-01 16:35:12 +0530

    create login succesly

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -1,16 +1,23 @@
-import React, {useEffect, useRef, useState}from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Container, Row, Col, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, login } from "../../../actions/userAction";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import './login.css'
 
 import loginImg from '../../../assets/images/login.png'
 import userIcon from '../../../assets/images/user.png'
 
 const Login = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  
+
+  const { error, loading, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
+
   const [loginNIC, setLoginNIC] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
@@ -18,6 +25,17 @@ const Login = () => {
     e.preventDefault();
     dispatch(login(loginNIC, loginPassword));
   };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, [dispatch, error, alert, isAuthenticated]);
 
   return (
     <Container>
@@ -35,10 +53,10 @@ const Login = () => {
 
               <Form onSubmit={loginSubmit}>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Control type="NIC" placeholder="Enter user NIC" value={loginNIC} onChange={(e)=>setLoginNIC(e.target.value)} />
+                  <Form.Control type="NIC" placeholder="Enter user NIC" value={loginNIC} onChange={(e) => setLoginNIC(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                  <Form.Control type="password" placeholder="Enter user Password" value={loginPassword} onChange={(e)=>setLoginPassword(e.target.value)} />
+                  <Form.Control type="password" placeholder="Enter user Password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
                 </Form.Group>
                 <Button className='btn secondary__btn auth__btn' type='submit'>Login</Button>
               </Form>
