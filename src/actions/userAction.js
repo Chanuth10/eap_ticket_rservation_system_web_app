38a9d74baa0b7203b8c55a020e5e649ad5663379
ShivaaniDushya <it20006884@my.sliit.lk>
2023-10-16 20:56:40 +0530

    user actions

---
+++ src/actions/userAction.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import {
+  CLEAR_ERRORS,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOAD_USER_FAIL,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+} from "../constans/userContants";
+
+// Login
+export const login = (NIC, Password) => async (dispatch) => {
+  console.log(NIC, Password);
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const { data } = await axios.post(
+      `http://localhost:5246/api/v2/admin/AuthAdmin`,
+      { nic: NIC, password: Password },
+      config
+    );
+    dispatch({ type: LOGIN_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.title });
+  }
+};
+
+// Register
+export const register =
+  (userName, nic, password, userType, email) => async (dispatch) => {
+    try {
+      dispatch({ type: REGISTER_USER_REQUEST });
+
+      const config = { headers: { "Content-Type": "application/json" } };
+
+      const { data } = await axios.post(
+        `/api/v2/UserManager`,
+        { userName, nic, password, userType, email },
+        config
+      );
+
+      dispatch({ type: REGISTER_USER_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: REGISTER_USER_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOAD_USER_REQUEST });
+    // eslint-disable-next-line
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.get(`/api/v2/me`);
+
+    dispatch({ type: LOAD_USER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
+  }
+};
+
+//   Clearing errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
